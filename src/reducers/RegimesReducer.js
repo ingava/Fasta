9273e0fd798da7969a9375a9a4b8064718a53505
Inga Vaiciakauskaite <i.vaiciakauskaite@gmail.com>
2017-08-20 11:03:58 +0300

    Complete regime selection sreen

--- src/reducers/RegimesReducer.js
+++ src/reducers/RegimesReducer.js
@@ -1,7 +1,24 @@
+import { REGIME_SELECTED, FASTING_START_SELECTED } from '../types';
+
+
 const initialState = {
-    regimes: '',
+    regime: null,
+    fastingStart: null,
 };
 
-export default (state = initialState) => {
-    return state;
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case REGIME_SELECTED:
+            return {
+                ...state,
+                regime: action.payload,
+            };
+        case FASTING_START_SELECTED:
+            return {
+                ...state,
+                fastingStart: action.payload,
+            };
+        default:
+            return state;
+    }
 }
--- src/screens/FastingRegime.js
+++ src/screens/FastingRegime.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View } from 'react-native';
 import { InstructionText, Card, CardSection, Button } from '../components/common/index';
 import { Actions } from 'react-native-router-flux';
+import { selectRegime } from '../actions/FastingRegime';
 
-const FastingRegime = () => {
-    const onButtonPress = () => {
+class FastingRegime extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleButtonPress = this.handleButtonPress.bind(this);
+    }
+
+    handleButtonPress(event) {
+        this.props.selectRegime(event.target.value);
         Actions.fastingStart()
     };
-    return (
-        <View>
-            <InstructionText>Choose your fasting interval</InstructionText>
-            <Card>
-                <CardSection>
-                    <Button onPress={onButtonPress}>14 - 10</Button>
-                </CardSection>
-                <CardSection>
-                    <Button onPress={() => {}}>16 - 8</Button>
-                </CardSection>
-                <CardSection>
-                    <Button onPress={() => {}}>18 - 6</Button>
-                </CardSection>
-                <CardSection>
-                    <Button onPress={() => {}}>24 hours</Button>
-                </CardSection>
-            </Card>
-        </View>
-    )
+    render() {
+        return (
+            <View>
+                <InstructionText>Choose your fasting interval</InstructionText>
+                <Card>
+                    <CardSection>
+                        <Button value={14} onPress={this.handleButtonPress}>14 hours</Button>
+                    </CardSection>
+                    <CardSection>
+                        <Button value={16} onPress={this.handleButtonPress}>16 hours</Button>
+                    </CardSection>
+                    <CardSection>
+                        <Button value={18} onPress={this.handleButtonPress}>18 hours</Button>
+                    </CardSection>
+                    <CardSection>
+                        <Button value={24} onPress={this.handleButtonPress}>24 hours</Button>
+                    </CardSection>
+                </Card>
+            </View>
+        )
+    }
 }
 
-export default FastingRegime
+export default connect(null, { selectRegime })(FastingRegime)
