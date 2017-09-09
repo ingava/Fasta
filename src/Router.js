251dc2c365249033c327236fd84887ccce964b52
Inga Vaiciakauskaite <i.vaiciakauskaite@gmail.com>
2017-09-09 13:30:28 +0300

    Add colors to header and its text

--- src/Router.js
+++ src/Router.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import FastingRegime from './screens/FastingRegime';
 import FastingStart from './screens/FastingStart';
+import { headerBackground, headerText } from './constants/colors';
 
 const RouterComponent = () => {
 
     return (
-        <Router sceneStyle={{ paddingTop: 0 }}>
-            <Scene key="fastingRegime" component={FastingRegime} title="Fasta" initial />
+        <Router>
+            <Scene key="fastingRegime" component={FastingRegime} navigationBarStyle={styles.navigationContainer} title="Fasta" titleStyle={styles.navigationText} initial />
             <Scene key="fastingStart" component={FastingStart} title="Fasta" />
         </Router>
     )
+};
+
+const styles = {
+    navigationContainer: {
+        backgroundColor: headerBackground,
+        borderBottomColor: 'white',
+    },
+    navigationText: {
+        fontFamily: 'notoserif',
+        color: headerText,
+        fontWeight: 'bold',
+        fontSize: 30,
+    }
 };
 
 export default RouterComponent
